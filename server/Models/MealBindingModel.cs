73e1175fcbe71d32b83fc2a75344983c732fa768
phamtruong7302 <truongpham7302@gmail.com>
2023-03-23 00:00:05 +0700

    MealController

---
+++ server/Models/MealBindingModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace server.Models
+{
+    public class MealBindingModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Price")]
+        public int Price { get; set; }
+        [Required]
+        [Display(Name = "AmountLeft")]
+        public int AmountLeft { get; set; }
+    }
+
+    public class MealEditBindingModel
+    {
+        [Required]
+        [Display(Name = "Price")]
+        public int Price { get; set; }
+        [Required]
+        [Display(Name = "AmountLeft")]
+        public int AmountLeft { get; set; }
+    }
+}
