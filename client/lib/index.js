ddc875391f6e098b30ffa4de801ebcb5deade677
Fiezt <dat09t@gmail.com>
2023-03-23 00:39:52 +0700

    update: lib

--- client/lib/index.js
+++ client/lib/index.js
@@ -1,45 +1,74 @@
-const { default: axios } = require('axios')
+export const tokenKey = "flashfood_token";
 
-const baseUrl = `${process.env.apiBaseUrl}`
-const defaultApiEndpoint = `${baseUrl}/api`
-const tokenKey = "flashfood_token";
-const getToken = () => localStorage.getItem(tokenKey);
-
-const controllers = {
-    account: `${defaultApiEndpoint}/Account`,
+export const controllers = {
+    account: `/api/Account`,
 }
 
 /**
- * @param {string} email
- * @param {string} password
- * @param {boolean} rememberMe
+ * 
+ * @param {import("axios").AxiosInstance} instance
+ * @returns
  */
-const login = async (email, password, rememberMe = false) => {
-    const token = await axios.post(`${controllers.account}/Login`, {
-        "Email": email,
-        "Password": password
-    }, {
-        headers: {
-            "Content-Type": "application/json"
-        }
-    }).then(r => r.data);
-    console.log(token);
-    localStorage.setItem(tokenKey, `${token}`);
+export default function api(instance) {
+    instance.defaults.headers.post["Content-Type"] = 'application/json';
+
+    /**
+     * 
+     * @param {string} email
+     * @param {string} password
+     * @param {boolean} rememberMe
+     */
+    const login = async (email, password, rememberMe = false) => {
+        const { data } = await instance.post(`${controllers.account}/Login`, {
+            "Email": email,
+            "Password": password
+        });
+        const token = `${data}`
+
+        localStorage.setItem(tokenKey, token);
+        setTokenToInstance(token)
+    }
+
+    function setTokenToInstance(token) {
+        instance.defaults.headers.common['Authorization'] = `Bearer ${token}`
+    }
+
+    function clearToken() {
+        delete instance.defaults.headers.common['Authorization']
+    }
+
+    const register = () => {
+
+    }
+
+    const logout = () => {
+        localStorage.removeItem(tokenKey);
+        clearToken();
+    }
+
+    const getUserInfo = async () => {
+        const { data } = await instance.get(`${controllers.account}/UserInfo`)
+        return data;
+    }
+
+    const routes = {
+        /**
+         * 
+         * @param {string} redirectTo
+         * @returns
+         */
+        login: (redirectTo = null) => redirectTo ? `/login?redirect=${redirectTo}` : `/login`,
+        forbidden: () => '/forbidden',
+    }
+
+    return {
+        login,
+        getUserInfo,
+        register,
+        logout,
+        clearToken,
+        setTokenToInstance,
+        routes
+    }
 }
 
-const register = () => {
-
-}
-
-const getUserInfo = async () => {
-    const token = getToken();
-    const res = await axios.get()
-}
-
-export {
-    login,
-    getToken,
-    register,
-    getUserInfo,
-    controllers
-}
