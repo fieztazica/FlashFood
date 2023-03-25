cca25b784ccf7cbd2a023cf27a8d2629f693f305
Fiezt <dat09t@gmail.com>
2023-03-25 23:19:27 +0700

    update: new pages

---
+++ client/pages/cart.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import AppLayout from '@/components/layouts/appLayout'
+import { Box, Button, Heading, Image } from '@chakra-ui/react'
+import { useAppStates } from '../lib/AppContext'
+
+function Cart() {
+    const { user } = useAppStates();
+    return (
+        <>
+            <Box>
+                
+            </Box>
+        </>
+    )
+}
+
+Cart.getLayout = function getLayout(page) {
+    return (
+        <AppLayout>
+            {page}
+        </AppLayout>
+    )
+}
+
+export default Cart
