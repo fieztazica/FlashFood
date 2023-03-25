e4982e763138fdb20013f1fd6d6b196aaf676682
Fiezt <dat09t@gmail.com>
2023-03-25 23:20:04 +0700

    fix: put controller to api controller

--- server/Controllers/api/CartItemController.cs
+++ server/Controllers/api/CartItemController.cs
@@ -7,7 +7,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
-namespace server.Controllers
+namespace server.Controllers.api
 {
     [Authorize]
     public class CartItemController : ApiController
