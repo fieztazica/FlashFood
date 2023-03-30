9281699ade589a07ee1f4c4f33d5d92ab1d406f8
vduong2k2 <76592891+vduong2k2@users.noreply.github.com>
2023-03-30 21:26:48 +0700

    update: UI

--- client/pages/cart.js
+++ client/pages/cart.js
@@ -1,28 +1,113 @@
 import Head from 'next/head'
 import AppLayout from '@/components/layouts/appLayout'
-import { Box, Button, Heading, Image } from '@chakra-ui/react'
+import {
+    Box,
+    Button,
+    Heading,
+    Image,
+    Card,
+    Stack,
+    CardBody,
+    CardFooter,
+    Text,
+    Checkbox,
+    Flex,
+    Spacer,
+} from '@chakra-ui/react'
 import { useAppStates } from '../lib/AppContext'
-
+import Item from '../components/Item'
+import React from 'react'
 function Cart() {
-    const { user, cart } = useAppStates();
-    return (
-        <>
-            <Box>
-                
-            </Box>
-            <Box>
+    const { user, cart } = useAppStates()
+    const [checkedItems, setCheckedItems] = React.useState(() =>
+        cart.map((i) => false)
+    )
+    const allChecked = checkedItems.every((i) => i === true)
+    const isIndeterminate = checkedItems.some((i) => i === true) && !allChecked
 
-            </Box>
-        </>
+    return (
+        <Box>
+            {!cart.length ? (
+                <Text>Your cart is empty!</Text>
+            ) : (
+                <>
+                    <Flex padding={5}>
+                        <Checkbox
+                            size={'lg'}
+                            isChecked={allChecked}
+                            isIndeterminate={isIndeterminate}
+                            onChange={(e) =>
+                                setCheckedItems((a) =>
+                                    new Array(a.length).fill(e.target.checked)
+                                )
+                            }
+                        >
+                            Select All
+                        </Checkbox>
+                        <Spacer />
+                        <Button>Buy</Button>
+                    </Flex>
+                    {cart.map((item, index) => (
+                        <Stack key={index} pl={6} mt={1} spacing={1}>
+                            <Checkbox
+                                isChecked={checkedItems[index]}
+                                onChange={(e) =>
+                                    setCheckedItems((arr) =>
+                                        arr.map((ele, i) => {
+                                            if (i === index) {
+                                                return e.target.checked
+                                            }
+                                            return ele
+                                        })
+                                    )
+                                }
+                            >
+                                <Card
+                                    direction={{ base: 'column', sm: 'row' }}
+                                    overflow="hidden"
+                                    variant="outline"
+                                >
+                                    <Image
+                                        objectFit="cover"
+                                        maxW={{ base: '100%', sm: '200px' }}
+                                        src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
+                                        alt="Caffe Latte"
+                                    />
+
+                                    <Stack>
+                                        <CardBody>
+                                            <Heading size="md">
+                                                The perfect latte
+                                            </Heading>
+
+                                            <Text py="2">
+                                                Caffè latte is a coffee beverage
+                                                of Italian origin made with
+                                                espresso and steamed milk.
+                                            </Text>
+                                        </CardBody>
+
+                                        <CardFooter>
+                                            <Button
+                                                variant="solid"
+                                                colorScheme="blue"
+                                            >
+                                                Buy Latte
+                                            </Button>
+                                        </CardFooter>
+                                    </Stack>
+                                </Card>
+                            </Checkbox>
+                        </Stack>
+                    ))}
+                </>
+            )}
+        </Box>
     )
 }
 
 Cart.getLayout = function getLayout(page) {
-    return (
-        <AppLayout>
-            {page}
-        </AppLayout>
-    )
+    return <AppLayout>{page}</AppLayout>
 }
 
 export default Cart
