1e918e2a4d15123ebbb348b000893f18f53dc01b
Fiezt <dat09t@gmail.com>
2023-04-02 20:24:46 +0700

    update: admin ui

--- server/Controllers/OrdersController.cs
+++ server/Controllers/OrdersController.cs
@@ -8,12 +8,18 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using server.Models;
+using server.ViewModels;
 
 namespace server.Controllers
 {
     public class OrdersController : Controller
     {
-        private ApplicationDbContext db = new ApplicationDbContext();
+        private readonly ApplicationDbContext db;
+
+        public OrdersController()
+        {
+            db = new ApplicationDbContext();
+        }
 
         // GET: Orders
         public ActionResult Index(string searchString)
@@ -63,25 +69,35 @@ namespace server.Controllers
             {
                 return HttpNotFound();
             }
-            ViewBag.UserId = new SelectList(db.Users, "Id", "FirstName", order.UserId);
+
+            var orderItemModels = db.OrderItems.Where(a => a.OrderId == order.Id).ToList();
+            List<OrderItemViewModel> orderItems = new List<OrderItemViewModel>();
+            foreach (var oi in orderItemModels)
+            {
+                oi.Meal = db.Meals.FirstOrDefault(m => m.Id == oi.MealId);
+                orderItems.Add(OrderItemViewModel.FromOrderItem(oi));
+            }
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Email", order.UserId);
+            ViewBag.OrderItems = orderItems;
             return View(order);
         }
-
 
         // POST: Orders/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,OrderAt,PaidAt,Total_money,Paid,Change,SellerId,UserId,Status")] Order order)
+        public ActionResult Edit([Bind(Include = "Id,OrderAt,Total_money,Paid,Change,SellerId,UserId,Status")] Order order)
         {
             if (ModelState.IsValid)
             {
+                if (order.Paid != null)
+                    order.PaidAt = DateTime.Now;
                 db.Entry(order).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            ViewBag.UserId = new SelectList(db.Users, "Id", "FirstName", order.UserId);
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Email", order.UserId);
             return View(order);
         }
 
@@ -99,7 +115,7 @@ namespace server.Controllers
                 return RedirectToAction("Index");
             }
 
-            ViewBag.UserId = new SelectList(db.Users, "Id", "FirstName", order.UserId);
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Email", order.UserId);
             return View(order);
         }
 
